0cc260cf883a86aed15e593cf3836837ff02e0d1
bloodray71 <109420133+bloodray71@users.noreply.github.com>
2023-05-24 23:38:19 +0300

    Create 13. Roman to Integer.js
    problem solving Create 13. Roman to Integer.js

---
+++ 13. Roman to Integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function(s) {
+     const romanSymbols = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  };
+
+  let result = 0;
+  let prev = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const current = romanSymbols[s[i]];
+
+    if (current < prev) {
+      result -= current;
+    } else {
+      result += current;
+    }
+
+    prev = current;
+  }
+
+  return result;
+};
